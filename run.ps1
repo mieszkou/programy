750c06bf8088d3568df4fd377cc8de368bf22bb9
mieszkou <mieszkou@gmail.com>
2026-07-14 13:03:41 +0200

    Add Get-File function for enhanced file downloading with progress tracking

--- run.ps1
+++ run.ps1
@@ -125,6 +125,64 @@ $jsonContent = @"
 # chatgpt 20250219
 
 function Get-File {
+        param (
+        [Parameter(Mandatory)]
+        [Alias("Url", "Uri", "Link", "Address")]
+        [string]$URL, 
+        
+        [Parameter(Mandatory)]
+        [Alias("OutFile", "Path", "Destination")]
+        [string]$File,
+
+        [Alias("Headers")]
+        [hashtable]$CustomHeaders
+    )
+
+    function Show-Progress {
+        param ([Parameter(Mandatory)][Single]$TotalValue, [Parameter(Mandatory)][Single]$CurrentValue, [Parameter(Mandatory)][string]$ProgressText, [Parameter()][int]$BarSize = 40, [Parameter()][switch]$Complete)
+        $percent = $CurrentValue / $TotalValue
+        $percentComplete = $percent * 100
+        if ($psISE) { Write-Progress "$ProgressText" -id 0 -percentComplete $percentComplete }
+        else { Write-Host -NoNewLine "`r$ProgressText $(''.PadRight($BarSize * $percent, [char]9608).PadRight($BarSize, [char]9617)) $($percentComplete.ToString('##0.00').PadLeft(6)) % " }
+    }
+    try {
+        $request = [System.Net.HttpWebRequest]::Create($URL)
+        if ($CustomHeaders) {
+            foreach ($header in $CustomHeaders.GetEnumerator()) {
+                switch ($header.Key.ToLowerInvariant()) {
+                    'referer' { $request.Referer = $header.Value; break }
+                    'user-agent' { $request.UserAgent = $header.Value; break }
+                    'accept' { $request.Accept = $header.Value; break }
+                    'content-type' { $request.ContentType = $header.Value; break }
+                    default { $request.Headers[$header.Key] = $header.Value }
+                }
+            }
+        }
+        $response = $request.GetResponse()
+        if ($response.StatusCode -eq 401 -or $response.StatusCode -eq 403 -or $response.StatusCode -eq 404) { throw "Remote file either doesn't exist, is unauthorized, or is forbidden for '$URL'." }
+        if ($File -match '^\.\\') { $File = Join-Path (Get-Location -PSProvider 'FileSystem') ($File -Split '^\.')[1] }
+        if ($File -and !(Split-Path $File)) { $File = Join-Path (Get-Location -PSProvider 'FileSystem') $File }
+        if ($File) { $fileDirectory = $([System.IO.Path]::GetDirectoryName($File)); if (!(Test-Path($fileDirectory))) { [System.IO.Directory]::CreateDirectory($fileDirectory) | Out-Null } }
+        [long]$fullSize = $response.ContentLength
+        [byte[]]$buffer = new-object byte[] 1048576
+        [long]$total = [long]$count = 0
+        $reader = $response.GetResponseStream()
+        $writer = new-object System.IO.FileStream $File, 'Create'
+        do {
+            $count = $reader.Read($buffer, 0, $buffer.Length)
+            $writer.Write($buffer, 0, $count)
+            $total += $count
+            if ($fullSize -gt 0) { Show-Progress -TotalValue $fullSize -CurrentValue $total -ProgressText "- Pobieranie: " }
+        } while ($count -gt 0)
+    }
+    finally {
+        $reader.Close()
+        $writer.Close()
+        Write-Host ""
+    }
+}
+
+function Get-File-Old {
     param (
         [Alias("Url", "Uri", "Link", "Address")]
         [string]$Source,
